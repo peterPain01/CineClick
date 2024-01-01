ef1211992c295cac3067194dcab1fb6df311280d
gigachad0209 <truongphat02092003@gmail.com>
2024-01-01 13:38:31 +0700

    update on recover

--- src/backend/controllers/Account.js
+++ src/backend/controllers/Account.js
@@ -2,23 +2,137 @@ const AccountModel = require("../models/Account");
 const bcrypt = require("bcrypt");
 const UserModel = require("../models/User");
 const saltround = 10;
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
+dotenv.config();
+const Utils = require("./Utils.js");
+var jwt = require("jwt-simple");
+
 module.exports = {
     async create(req, res, next) {
-        const {email, password} = req.body;
-        console.log(`[INFO] Register api call with for`, email);
+        const { email, password } = req.body;
+        console.log([INFO] Register api call with for, email);
         try {
             if (!email || !password) {
                 res.status(400).send("Missing one of password/email");
-            } else if (await AccountModel.get(email) != null) {
+            } else if ((await AccountModel.get(email)) != null) {
                 res.status(400).send("Email is already registered");
             } else {
                 const encrypted_pass = await bcrypt.hash(password, saltround);
-                await AccountModel.insert(new AccountModel.AccountInfo(email, encrypted_pass, "free-viewer"));
-                await UserModel.insert(new UserModel.UserInfo(email, null, null));
+                await AccountModel.insert(
+                    new AccountModel.AccountInfo(
+                        email,
+                        encrypted_pass,
+                        "free-viewer"
+                    )
+                );
+                await UserModel.insert(
+                    new UserModel.UserInfo(email, null, null)
+                );
                 res.status(200).send("OK");
             }
         } catch (err) {
             next(err);
         }
     },
+
+    async sendMailActive(req, res, next) {
+        const { email } = req.body;
+
+        try {
+            const acc = await AccountModel.get(email);
+            if (acc) {
+                var payload = {
+                    email: email,
+                };
+                var secret = acc.password + "-" + acc.email;
+                var token = jwt.encode(payload, secret);
+
+                // TODO change hard url to .env
+                const info = await sendEmail(
+                    email,
+                    "Reset Password",
+                    http://localhost:5173/reset-password/${email}/${token}
+                );
+                res.status(200).send(
+                    "Check your email for instructions to reset your password."
+                );
+            } else {
+                res.status(404).send("Email not found");
+            }
+        } catch (err) {
+            next(err);
+        }
+    },
+
+    async resetPassword(req, res, next) {
+        const { email, token, password } = req.body;
+        if (!password || typeof password !== "string") {
+            res.status(400).send("Error occurred ! Please try again");
+        }
+        try {
+            const acc = await AccountModel.get(email);
+            if (acc) {
+                let secret = acc.password + "-" + acc.email;
+                let payload = "";
+                try {
+                    payload = await jwt.decode(token, secret);
+                } catch (err) {
+                    res.status(400).send(
+                        "That link expired! Please send another request to get new link"
+                    );
+                }
+                const encrypted_pass = await bcrypt.hash(password, saltround);
+                const result = await AccountModel.update(email, encrypted_pass);
+                if (result == null) {
+                    res.status(500).send(
+                        "Update password failed ! Please try again"
+                    );
+                }
+                res.status(200).send("Reset Password Successfully");
+            } else {
+                res.status(404).send("Email not found");
+            }
+        } catch (err) {
+            next(err);
+        }
+    },
+};
+
+async function sendEmail(to, subject, resetPasswordLink) {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+    let htmlTemplate = "";
+    const filePath = path.join(__dirname, "recoverMail.html");
+    try {
+        htmlTemplate = fs.readFileSync(filePath, "utf8");
+    } catch (err) {
+        console.error("Read html err:", err.message);
+    }
+
+    const formattedHTML = htmlTemplate.replace(
+        /{resetPasswordLink}/g,
+        resetPasswordLink
+    );
+    const mailOptions = {
+        from: process.env.EMAIL_USERNAME,
+        to: to,
+        subject: subject,
+        html: formattedHTML,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            throw error;
+        } else {
+            return info.response;
+        }
+    });
 }
--- src/backend/models/Account.js
+++ src/backend/models/Account.js
@@ -2,7 +2,11 @@ const db = require("../modules/Database");
 
 module.exports = {
     is_valid_type(type) {
-        if (type !== 'admin' && type !== 'free-viewer' && type !== 'premium-viewer') {
+        if (
+            type !== "admin" &&
+            type !== "free-viewer" &&
+            type !== "premium-viewer"
+        ) {
             return false;
         }
         return true;
@@ -15,7 +19,7 @@ module.exports = {
             this.email = email;
             this.password = password;
             if (!module.exports.is_valid_type(type)) {
-                throw new Error(`Invalid account type ${type}`);
+                throw new Error(Invalid account type ${type});
             }
             this.type = type;
         }
@@ -23,25 +27,40 @@ module.exports = {
     // NOTE: 'type' is optional
     async get(email, type) {
         let condition = "";
-        if (email) condition += ` email = '${email}'`;
+        if (email) condition +=  email = '${email}';
         else throw new Error("Email is missing");
         if (type) {
             if (!this.is_valid_type(type)) {
-                throw new Error(`Unregconized type '${type}'`);
+                throw new Error(Unregconized type '${type}');
             }
-            condition += ` type = '${type}'`;
+            condition +=  type = '${type}';
         }
         let result = await db.get("Account", condition);
         if (result == null) return null;
         {
-            const {email, password, type} = result;
+            const { email, password, type } = result;
             return new this.AccountInfo(email, password, type);
         }
     },
     async insert(acc_info) {
         if (!(acc_info instanceof this.AccountInfo)) {
-            throw new Error("Can't insert object of different type than AccountInfo to 'Account' table");
+            throw new Error(
+                "Can't insert object of different type than AccountInfo to 'Account' table"
+            );
         }
         await db.helper_insert("Account", acc_info);
-    }
+    },
+
+    async update(email, password) {
+        let condition = "";
+        let update = "";
+        if (email) condition +=  email = '${email}';
+        else throw new Error("Email is missing");
+        if (password) {
+            update +=  password = '${password}';
+        }else throw new Error("Password is missing");
+        
+        let result = await db.update("Account", condition, update);
+        return result
+    },
 };
